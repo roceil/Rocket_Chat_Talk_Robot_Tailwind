57504c97cad65a7ec765c1a7bfc0ce68ea8ba534
roceil <a0978006326@gmail.com>
2022-10-14 00:17:27 +0800

    Update 2022-10-13T16:17:19.575Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -47,4 +47,19 @@ $(document).ready(function () {
     $("#toggle").toggle();
   });
 });
+$(function () {
+  $(".question h3").click(function (e) {
+    e.preventDefault(); //在 h2 上動態新增 class
+
+    $(this).toggleClass("active"); //h2 在父層元素中，找到 p 元素，並且給它滑動效果
+
+    $(this).parent().find("p").slideToggle(); //找到 img 元素，並且更改src
+
+    $(this).find("img").attr("src", "https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon_minus.svg"); //h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 p 標籤，並向上收闔
+
+    $(this).parent().siblings().find("p").slideUp(); //h2 在父層 .question 元素中，找到其他 .question 同層元素，再找到該同層元素的 h2 標籤，並動態移除 class
+
+    $(this).parent().siblings().find("h3").removeClass("active");
+  });
+});
 //# sourceMappingURL=all.js.map
