3256132bfbcd19413b0c0ccf6eff7ef95976709b
roceil <a0978006326@gmail.com>
2022-10-13 17:35:40 +0800

    less js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,48 @@
-console.log('Hello!');
+// 彈性選擇區塊
+// DOM
+const numBtn = document.querySelector(`#numBtn`);
+const userNum = document.querySelectorAll(`#userNum`);
+const monthNum = document.querySelectorAll(`#monthNum`);
+const unit = document.querySelectorAll(`#unit`);
+const problemList = document.querySelectorAll(`#problemList`)
+console.log(problemList);
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+// 宣告
+let userStr;
+let monthStrBasic;
+let monthStrFormal;
+let nanStr;
+
+// 數字按鈕監聽
+numBtn.addEventListener("click", function (e) {
+  userStr = e.target.value;
+  monthStrBasic = e.target.value * 0.06;
+  monthStrFormal = e.target.value * 0.16;
+  nanStr = `秘密`;
+  if (e.target.value === undefined) {
+    return;
+  } else {
+    userNum[0].textContent = userStr;
+    userNum[1].textContent = userStr;
+    if (e.target.value === `>25000`) {
+      monthNum[0].textContent = nanStr;
+      monthNum[1].textContent = nanStr;
+      unit[0].setAttribute("class", `hidden`);
+      unit[1].setAttribute("class", `hidden`);
+      return;
+    } else {
+      monthNum[0].textContent = monthStrBasic;
+      monthNum[1].textContent = monthStrFormal;
+      unit[0].setAttribute("class", `text-xs leading-[1.41666]  pt-[10px]`);
+      unit[1].setAttribute("class", `text-xs leading-[1.41666]  pt-[10px]`);
+      return;
+    }
+  }
+});
+
+// jQuery
+$(document).ready(function () {
+  $(`#problemList`).click(function (e) { 
+    $(`#toggle`).toggle();
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,59 +2,75 @@ module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
     extend: {
-      colors : {
-        'chatRobot' : {
-          primary : '#56C4C5',
-          secondary : '#F5CA52',
-          warning : '#FF5D50',
-          black : '#221E1F',
-          gray : '#747474',
-          light_gray :'#F5F5F5' 
-        }
+      colors: {
+        chatRobot: {
+          primary: "#56C4C5",
+          secondary: "#F5CA52",
+          warning: "#FF5D50",
+          black: "#221E1F",
+          gray: "#747474",
+          light_gray: "#F5F5F5",
+        },
       },
-      backgroundImage : {
-        'ball1' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball01.svg')",
-        'ball2' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball02.svg')",
-        'ball3' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball03.svg')",
-        'banner' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/bg_home.svg')",
-        'banner-sm' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/bg_home_sm.svg')",
-        'icon01' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon01.svg')",
-        'icon02' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon02.svg')",
-        'icon03' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon03.svg')",
-        'icon04' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon04.svg')",
-        'user01' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user01.jpg')",
-        'user02' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user02.jpg')",
-        'user03' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user03.jpg')",
-        'footer' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/home_footer.svg')",
-        'footer-sm' : "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/footer_sm.svg')",
+      backgroundImage: {
+        ball1:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball01.svg')",
+        ball2:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball02.svg')",
+        ball3:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/ball03.svg')",
+        banner:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/bg_home.svg')",
+        "banner-sm":
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/bg_home_sm.svg')",
+        icon01:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon01.svg')",
+        icon02:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon02.svg')",
+        icon03:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon03.svg')",
+        icon04:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/icon04.svg')",
+        user01:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user01.jpg')",
+        user02:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user02.jpg')",
+        user03:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/user03.jpg')",
+        footer:
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/home_footer.svg')",
+        "footer-sm":
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/footer_sm.svg')",
+        "charge-banner-sm":
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/banner_sm.svg')",
+        "charge-banner":
+          "url('https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/chatTalker_images/banner.svg')",
       },
-    },  
+    },
     container: {
       center: true,
-      padding: '20px',
+      padding: "20px",
     },
-    fontFamily:{
-      'noto' : ['Noto Sans TC', 'sans-serif']
+    fontFamily: {
+      noto: ["Noto Sans TC", "sans-serif"],
+      wendy: ["Wendy One", "Noto Sans TC", "sans-serif"],
     },
     screens: {
-      'sm': '376px',
+      sm: "376px",
       // => @media (min-width: 376px) { ... }
 
-      'sm-md': '500px',
+      "sm-md": "500px",
       // => @media (min-width: 500px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1128px',
+      xl: "1128px",
       // => @media (min-width: 1128px) { ... }
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
