100c93f53a2f642f13d8b72c08b93be59f0e76bd
roceil <a0978006326@gmail.com>
2022-10-13 17:35:13 +0800

    Update 2022-10-13T09:35:11.163Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,50 @@
 "use strict";
 
-console.log('Hello!');
+// 彈性選擇區塊
+// DOM
+var numBtn = document.querySelector("#numBtn");
+var userNum = document.querySelectorAll("#userNum");
+var monthNum = document.querySelectorAll("#monthNum");
+var unit = document.querySelectorAll("#unit");
+var problemList = document.querySelectorAll("#problemList");
+console.log(problemList); // 宣告
+
+var userStr;
+var monthStrBasic;
+var monthStrFormal;
+var nanStr; // 數字按鈕監聽
+
+numBtn.addEventListener("click", function (e) {
+  userStr = e.target.value;
+  monthStrBasic = e.target.value * 0.06;
+  monthStrFormal = e.target.value * 0.16;
+  nanStr = "\u79D8\u5BC6";
+
+  if (e.target.value === undefined) {
+    return;
+  } else {
+    userNum[0].textContent = userStr;
+    userNum[1].textContent = userStr;
+
+    if (e.target.value === ">25000") {
+      monthNum[0].textContent = nanStr;
+      monthNum[1].textContent = nanStr;
+      unit[0].setAttribute("class", "hidden");
+      unit[1].setAttribute("class", "hidden");
+      return;
+    } else {
+      monthNum[0].textContent = monthStrBasic;
+      monthNum[1].textContent = monthStrFormal;
+      unit[0].setAttribute("class", "text-xs leading-[1.41666]  pt-[10px]");
+      unit[1].setAttribute("class", "text-xs leading-[1.41666]  pt-[10px]");
+      return;
+    }
+  }
+}); // jQuery
+
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  $("#problemList").click(function (e) {
+    $("#toggle").toggle();
+  });
 });
 //# sourceMappingURL=all.js.map
